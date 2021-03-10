f4618ee29c032ea2004ddf13a07f0d93a2d4893a
Mohsin Raza <mohsin.doer@gmail.com>
2021-03-11 01:26:06 +0500

    'as'

--- flaskapp.py
+++ flaskapp.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, request
-from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
-run_with_ngrok(app)  # Start ngrok when app is run
+
 
 def parse_command(previous_command, current_command):
 
